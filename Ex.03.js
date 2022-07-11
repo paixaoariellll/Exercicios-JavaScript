b1082045a2a46d84ca21c7db5ed4c7a27dbebd42
Ariel Paixão <71910000+paixaoariellll@users.noreply.github.com>
2022-07-11 18:21:46 -0300

    lição 03

---
+++ Ex.03.js
@@ -0,0 +1,20 @@
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70.
+
+/* 
+ Exemplo: 
+ let count = 5
+ count = count + 1
+ console.log(count) 
+*/
+
+let bonusPoints = 50 
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
