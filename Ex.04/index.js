04c7ab3cd13c7d33ab91559f6bb9c65b140dee99
paixaoariellll <paixaoariellll@gmail.com>
2022-07-11 18:23:22 -0300

    lição 04

---
+++ Ex.04/index.js
@@ -0,0 +1,8 @@
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+function increment() {
+    console.log("The button was clicked")
+}
