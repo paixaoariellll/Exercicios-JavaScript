63a5f1ba48ae7b5b15a7cdcf33bc2e92bc451acf
Ariel Paixão <71910000+paixaoariellll@users.noreply.github.com>
2022-07-11 18:21:19 -0300

    Lição 02

---
+++ Ex.02.js
@@ -0,0 +1,19 @@
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+/* 
+Exemplo:
+let firstbatch = 5
+let secoundBatch = 7
+let count = firstbatch + secoundBatch 
+console.log (count) 
+*/
+
+
+let myAge = 26 
+let humanDogRatio = 7
+
+let myDogAge = myAge + humanDogRatio
+
+console.log(myDogAge)
