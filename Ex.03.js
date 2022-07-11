ac681a3d988ff84829c11b6758bcc208cf7ba397
Ariel Paixão <71910000+paixaoariellll@users.noreply.github.com>
2022-07-11 18:23:22 -0300

    lição 03

--- Ex.03.js
+++ Ex.03.js
@@ -1,5 +1,6 @@
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70.
+// Console.log the value after each step
 
 /* 
  Exemplo: 
