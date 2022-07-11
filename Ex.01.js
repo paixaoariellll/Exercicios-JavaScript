21e52b1b8eed5b05d0df252053fec94ff01f5677
paixaoariellll <paixaoariellll@gmail.com>
2022-07-11 18:06:16 -0300

    lição 01

---
+++ Ex.01.js
@@ -0,0 +1,15 @@
+/*
+Exercício proposto:
+0. Create a count and show it.
+
+1. Create a variable, myAge, and set its value to your age.
+
+2. Log the myAge variable to the console. 
+*/
+
+
+let count = 5
+console.log(count)
+
+let myAge = 35
+console.log(myAge)
