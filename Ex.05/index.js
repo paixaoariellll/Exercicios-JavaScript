9025c31fe634358af701f329aa5f1b39745192b3
paixaoariellll <paixaoariellll@gmail.com>
2022-07-11 19:13:15 -0300

    lição 05

---
+++ Ex.05/index.js
@@ -0,0 +1,20 @@
+// Setting up the the race 🏎 🏎 🏎
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+function countdown(){
+    for ( i = 5; i >= 0; i -- ){
+        console.log(i)
+    }
+}
+
+countdown()
+
+// Get ready for a new race therefore 🏎 🏎 🏎
+/* function countup(){
+    for ( i = 0; i >= 5; i ++ ){
+        console.log(i)
+    }
+}
+countup() */
