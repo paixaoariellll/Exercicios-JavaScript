6ed1d97c8fd17772f817e9eacb130c4e431d3981
paixaoariellll <paixaoariellll@gmail.com>
2022-07-12 15:11:29 -0300

    lição 12

---
+++ Ex.12/index.js
@@ -0,0 +1,10 @@
+const userName = "per"
+
+// Create a variable, message, that stores the string: "You have tree new notifications"
+// Create a variable, messageToUser, that contains the message we have logged
+
+const message = "You have tree new notifications"
+
+const messageToUser = message + ", " + userName + "!"
+
+console.log(messageToUser)
