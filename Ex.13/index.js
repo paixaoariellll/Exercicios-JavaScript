1b9f105961b314438a510608f1b55db689b45872
paixaoariellll <paixaoariellll@gmail.com>
2022-07-12 15:16:41 -0300

    lição 13

---
+++ Ex.13/index.js
@@ -0,0 +1,11 @@
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console.
+
+const myName = "Per Ariel Paixão"
+const greeting = "Hi, my name is "
+const myGreeting = greeting + myName
+
+console.log(myGreeting)
