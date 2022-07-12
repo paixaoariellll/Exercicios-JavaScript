5044e9ce09663da2ce00e64fdd548279c46098a3
paixaoariellll <paixaoariellll@gmail.com>
2022-07-12 15:24:23 -0300

    lição 14

---
+++ Ex.14/index.js
@@ -0,0 +1,13 @@
+// learning the diferences between sum and increase
+
+let points = 4
+let bonusPoints = "10"
+
+let totalPoints = points + bonusPoints
+
+console.log(totalPoints) // increase = 410
+
+console.log(4 + 5) // sum = 9
+console.log("2" + "4") // increase = 24
+console.log("5" + 1) // increase = 51
+console.log(100 + "100") // increase = 100100
